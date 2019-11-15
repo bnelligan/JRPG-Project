ea29d31664396041a2e481e8c215a706d4a018f5
Brendan Nelligan <nelligan81@gmail.com>
2019-11-14 19:05:45 -0500

    Move unit stats UI to the top and add BaseUnit script

---
+++ Assets/Scripts/BaseUnit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseUnit
+{
+    public string Name { get; protected set; }
+    public int HP { get; protected set; }
+    public int MP { get; protected set; }
+    public uint Level { get; protected set; }
+    public bool IsDead { get; protected set; }
+
+    
+    public virtual void TakeDamage(int damage, BaseUnit source)
+    {
+        HP -= damage;
+        if(HP <= 0)
+        {
+            Die(source);
+        }
+    }
+
+    public virtual void Die(BaseUnit Source)
+    {
+        Debug.Log($"{Name} is Dead!");
+        IsDead = true;
+        HP = 0;
+    }
+}
