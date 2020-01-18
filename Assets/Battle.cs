ae1752033743a2b094489dc3a9838ffb88298151
Brendan Nelligan <nelligan81@gmail.com>
2020-01-17 22:11:39 -0500

    Tilemap, basic grass and dirt tiles, and a few mock scenes for testing

--- Assets/Battle.cs
+++ Assets/Battle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Battle : MonoBehaviour
 {
     [SerializeField]
---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject BasicFloor;
+    public GameObject BasicWall;
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateBasicLevel()
+    {
+
+    }
+}
