a1703ba8f2e2334a3d1bb05409aff2bac9e3b88c
Brendan Nelligan <nelligan81@gmail.com>
2020-01-21 08:17:51 -0500

    Draw heart and stam HUD icons from real UnitStats

--- Assets/CharacterHUD.cs
+++ Assets/CharacterHUD.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(UnitStats))]
 public class CharacterHUD : MonoBehaviour
 {
-    string heartIconPath = "Sprites/Heart.png";
-    string stamIconPath = "Sprites/Stam.png";
-    GameObject HUD_Group;
+    // Icon Paths
+    string baseIconPrefabPath = "Prefabs/Icon";
+    string heartIconPath = "Sprites/Heart";
+    string stamIconPath = "Sprites/Stam";
 
+    // Private objects
     [SerializeField]
-    float IconOffsetX = 0.5625f;
-    [SerializeField]
-    float IconOffsetY = 1.3125f;
+    GameObject HUD_Group;
+    GameObject BaseIconPrefab;
+    UnitStats targetStats;
+
+    List<GameObject> heartList;
+    List<GameObject> stamList;
+
+    // HUD Parameters
+    public float IconOffsetX { get; private set; } = 0.625f;
+    public float IconOffsetY { get; private set; } = 0.625f;
+    
+    int heartsRow = 1;
+    int stamRow = 0;
 
     public bool RefreshHUD = true;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetStats = GetComponent<UnitStats>();
+        BaseIconPrefab = Resources.Load<GameObject>(baseIconPrefabPath);
+        if(BaseIconPrefab == null)
+        {
+            Debug.LogError("Invalid base icon prefab: " + baseIconPrefabPath);
+        }
     }
 
     // Update is called once per frame
@@ -32,13 +49,42 @@ public class CharacterHUD : MonoBehaviour
         }
     }
 
-    public void DrawHUD()
+    private void DrawHUD()
     {
-        
+        DrawHearts();
+        DrawStam();
     }
 
     private void DrawHearts()
     {
+        int numHearts = targetStats.HP;
+        heartList = DrawIcons(heartIconPath, heartsRow, numHearts);
+    }
+    private void DrawStam()
+    {
+        int numStam = targetStats.MP;
+        stamList = DrawIcons(stamIconPath, stamRow, numStam);
+    }
 
+    private List<GameObject> DrawIcons(string iconSpritePath, int row, int count)
+    {
+        Vector3 startPosition = new Vector3((1f - count) / 2f * IconOffsetX, IconOffsetY * row, HUD_Group.transform.position.z);
+        Sprite iconSprite = Resources.Load<Sprite>(iconSpritePath);
+        List<GameObject> iconsDrawn = new List<GameObject>();
+        if (iconSprite != null)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Vector3 pos = startPosition + new Vector3((IconOffsetX * i), 0, 0);
+                GameObject iconInstance = GameObject.Instantiate(BaseIconPrefab, HUD_Group.transform);
+                iconInstance.transform.localPosition = pos;
+                iconInstance.GetComponent<SpriteRenderer>().sprite = iconSprite;
+            }
+        }
+        else
+        {
+            Debug.LogError("Invalid icon path: " + iconSpritePath);
+        }
+        return iconsDrawn;
     }
 }
